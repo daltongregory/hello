540ea6e0bda3f0ae6b6b51de4745d821014d93dd
Dalton Gregory <dgregory3@huskers.unl.edu>
2019-08-29 15:06:44 -0500

    Added major
    addded major to the hello filel

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Nolan and Dalton
+ * Date: 2019/27/08
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+	
+  printf("Dalton is the best!\n");
+  printf("Dalton is majoring in Computer Science!\n");
+  
+  return 0;
+}
